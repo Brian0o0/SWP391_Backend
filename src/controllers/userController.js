b261a5cb34d3a97ede33d94dcb9cb198d462aa50
Brian0o0 <hoangtronghuy.KT@gmail.com>
2024-05-28 02:19:53 +0700

    login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,49 +1,58 @@
 const express = require('express');
-const { getUser, getUserById, updateUserById, deleteUserById, insertUser, checkEmailValid, getUserByUserName } = require('../controllers/homeController');
+const { getUser, getUserById, updateUserById, deleteUserById, insertUser, getUserByUserName } = require('../controllers/homeController');
+const bcrypt = require('bcrypt');
 
 
 const createUserController = async (req, res) => {
     const { PassWord, Name, Phone, Address, Email, UserName } = req.body;
-    const user = {
-        PassWord: PassWord,
-        Name: Name,
-        Phone: Phone,
-        Address: Address,
-        Email: Email,
-        UserName: UserName
-    };
     let userNameTemp = await getUserByUserName(UserName);
     // let emailTemp = await checkEmailValid(Email);
     try {
         if (PassWord && Name && Phone && Address && Email && UserName) {
-            // const isEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(Email);
-            // const isCheckEmail = emailTemp;
-            const isUserId = userNameTemp;
-            if (isUserId.length > 0) {
-                return res.json({
-                    status: 'err',
-                    message: 'The user name is existed!!!'
-                })
-            } else {
-                if (user.Phone.length == 10) {
-                    const checkInser = await insertUser(user);
-                    if (checkInser) {
-                        return res.json({
-                            status: 'success',
-                            message: 'User created successfully'
-                        });
+            const isEmail = /^[\w._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(Email);
+            if (isEmail) {
+                const isUserName = userNameTemp;
+                if (isUserName.length > 0) {
+                    return res.json({
+                        status: 'err',
+                        message: 'The user name is existed!!!'
+                    })
+                } else {
+                    if (Phone.length == 10) {
+                        const hashPassword = bcrypt.hashSync(PassWord, 10);
+                        console.log(hashPassword);
+                        const user = {
+                            PassWord: hashPassword,
+                            Name: Name,
+                            Phone: Phone,
+                            Address: Address,
+                            Email: Email,
+                            UserName: UserName
+                        };
+                        const checkInser = await insertUser(user);
+                        if (checkInser) {
+                            return res.json({
+                                status: 'success',
+                                message: 'User created successfully'
+                            });
+                        } else {
+                            return res.json({
+                                status: 'err',
+                                message: 'User created fail'
+                            });
+                        }
                     } else {
                         return res.json({
                             status: 'err',
-                            message: 'User created fail'
+                            message: 'Invalid phone number'
                         });
                     }
-                } else {
-                    return res.json({
-                        status: 'err',
-                        message: 'Invalid phone number'
-                    });
                 }
+            } else {
+                return res.json({
+                    status: 'err',
+                    message: 'The email invalid'
+                })
             }
         } else {
             return res.json({
@@ -60,45 +69,45 @@ const createUserController = async (req, res) => {
     }
 }
 
+
+
 const login = async (req, res) => {
-    // const { UserID, Email, UserName } = req.body;
-
-    // let userIdTemp = await getUserById(UserID);
-    // let emailTemp = await checkEmailValid(Email);
-    // try {
-    //     if ((PassWord && Email) || (UserName && PassWord)) {
-    //         const isEmail = /^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/.test(Email);
-    //         if (isEmail) {
-    //             const isCheckEmail = userIdTemp;
-    //             const isUserId = emailTemp;
-    //             if (isCheckEmail != null || isUserId != null) {
-    //                 return res.json({
-    //                     status: 'err',
-    //                     message: 'The name or user name is existed!!!'
-    //                 })
-    //             } else {
-
-    //             }
-
-    //         } else {
-    //             return res.json({
-    //                 status: 'err',
-    //                 message: 'user name is not a email'
-    //             })
-    //         }
-    //     } else {
-    //         return res.json({
-    //             status: 'err',
-    //             message: 'UserID PassWord Name Phone Address Email Role and UserName is required'
-    //         })
-    //     }
-    // } catch (err) {
-    //     console.log(err);
-    //     return res.json({
-    //         status: 'err',
-    //         message: err.message
-    //     })
-    // }
+    const { PassWord, UserName } = req.body;
+    let userIdTemp = await getUserByUserName(UserName);
+    try {
+        if (PassWord && UserName) {
+            if (userIdTemp.length <= 0) {
+                return res.json({
+                    status: 'err',
+                    message: 'Account does not exist'
+                })
+            } else {
+                const checkPassword = bcrypt.compareSync(PassWord, userIdTemp[0].PassWord);
+                if (checkPassword) {
+                    return res.json({
+                        status: 'success',
+                        message: 'login successful'
+                    })
+                } else {
+                    return res.json({
+                        status: 'err',
+                        message: 'Incorrect password'
+                    })
+                }
+            }
+        } else {
+            return res.json({
+                status: 'err',
+                message: 'UserName and PassWord is required'
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return res.json({
+            status: 'err',
+            message: err.message
+        })
+    }
 }
 module.exports = {
     createUserController, login
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ let app = express();
 let port = process.env.POST || 8081;
 let hostname = process.env.HOST_NAME;
 const webRouter = require('./routers/api');
+const session = require('express-session');
+
 app.use(express.json()) // for json
 
 /*Dòng mã app.use(express.json()) trong ứng dụng Express có tác dụng thiết lập middleware để phân tích cú pháp (parse) các yêu cầu HTTP với payload JSON. 
@@ -11,7 +13,11 @@ app.use(express.json()) // for json
  middleware này sẽ tự động phân tích cú pháp dữ liệu JSON đó và làm cho dữ liệu có thể truy cập được thông qua req.body. */
 
 
-
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 
 
 app.use('/', webRouter);
